857ee8b2f178076f10108272b2551ea4c8cb5c96
LeishangthemSantosh <84895858+LeishangthemSantosh@users.noreply.github.com>
2022-09-14 12:34:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           
-          Dayal <code>src/App.js</code> and save to reload.
+          This Is React Js
         </p>
         <a
           className="App-link"
